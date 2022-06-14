ea82d9443d92de16e527ab0586273664ee805633
Ray Ortiz <oray@bgsu.edu>
2022-06-14 07:38:01 -0400

    Export Statements

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const API_KEY = 'AIzaSyAmvA_sACIFT9v6T1iWnsK6IXyb6XSN4Eo';
+import SearchBar from "./components/search_bar";
+
+const API_KEY = "AIzaSyAmvA_sACIFT9v6T1iWnsK6IXyb6XSN4Eo";
 
 // Create a new component. This component should produce
 // some HTML
 // "Fat Arrow" aka => replaces function
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 // Take this component's generated HTML and put it
